d906ad21305e839f129916195df49b0a804798c7
zhy <15937367035@163.com>
2019-07-30 10:00:34 +0800

    尝试搭建

---
+++ book/02-javaScript advanced programming -Professional JavaScript for Web Developers/chapter one/test.js
@@ -0,0 +1,2 @@
+console.log("小小的测试");
+//运行方式 node test.js
---
+++ demo-vue-example/src/views/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  components: {
+    HelloWorld
+  }
+};
+</script>
+<style lang="less" scoped>
+.a {
+    background:#000;
+  .b {
+      color: #fff;
+  }
+}
+</style>
+
+
+
